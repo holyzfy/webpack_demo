077a181f57a430d5357f32c538d0183f1c3af730
zhaofuyun <zhaofuyun202@gmail.com>
2016-06-13 13:26:53 +0800

    inject js files to the bottom of the body element

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 var autoprefixer = require('autoprefixer');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        head_static: './inc/head_static.html',
-        form: ['./js/form.js', './form.html']
+        form: './js/form.js',
+        list: './js/list.js'
     },
     output: {
         path: './dist',
         publicPath: 'http://static.example.com/webpack_demo/',
-        filename: '[name].[chunkhash:7].js',
-        chunkFilename: '[name].[chunkhash:7].js'
+        filename: 'js/[name].[chunkhash:7].js',
+        chunkFilename: 'js/[name].[chunkhash:7].js'
     },
     module: {
         loaders: [
             {
                 test: /\.html$/,
                 loaders: [
-                    'file?name=[path][name].[ext]',
                     'extract',
                     'html?' + JSON.stringify({
                         attrs: ['img:src', 'link:href']
@@ -51,6 +51,23 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(['dist'], {
             verbose: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'inc/head_static.html',
+            template: 'html!inc/head_static.html',
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'form.html',
+            template: 'html!form.html',
+            chunks: ['form'],
+            inject: 'body'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'list.html',
+            template: 'html!list.html',
+            chunks: ['list'],
+            inject: 'body'
         })
     ]
 };
